85e1d1327a05c2edd92f0a355d1379c50ed794d8
Avius <avius.ravis@gmail.com>
2023-02-27 20:30:17 +0100

    added example load script command

---
+++ source/ECF/Example/Commands/LoadScriptExampleCommand.cs
@@ -0,0 +1,29 @@
+﻿using ECF;
+using ECF.Engine;
+using ECF.Utilities;
+using Example.Properties;
+
+namespace Example.Commands
+{
+    [Command("load-example")]
+    [CmdDescription("loads example script from resources")]
+    public class LoadScriptExampleCommand : CommandBase
+    {
+        private readonly InterfaceContext interfaceContext;
+
+        public LoadScriptExampleCommand(InterfaceContext interfaceContext)
+        {
+            this.interfaceContext = interfaceContext;
+        }
+
+        public override void Execute()
+        {
+            var loader = new ScriptLoader(interfaceContext);
+
+            using (var reader = new StringReader(Resources.ExampleSctipt))
+            {
+                loader.Load(reader);
+            }
+        }
+    }
+}
