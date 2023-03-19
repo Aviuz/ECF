17e002861f34205107f14289673fbb36213ea6d8
Avius <avius.ravis@gmail.com>
2023-03-19 15:29:32 +0100

    minor

--- source/Example/Program.cs
+++ source/Example/Program.cs
@@ -5,7 +5,7 @@ new ECFHostBuilder()
     .AddConfiguration(optional: true) // adds appsettings.json        
     .Configure((ctx, services, _) =>
     {
-        ctx.Intro = $"This is example console application based on ECF v.{typeof(Program).Assembly.GetName().Version}\nType help to list available commands";
+        ctx.Intro = $"This is example console application based on ECF. Version {typeof(Program).Assembly.GetName().Version}.\nType help to list available commands";
         ctx.HelpIntro = "Welcome to example program that showcases ECF framework. Enter one of command listed below";
         ctx.Prefix = ">";
     })
