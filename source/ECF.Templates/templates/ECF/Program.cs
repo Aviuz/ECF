a2e2ae2b3972e50be1da5c2f06782a0d15b763d6
Avius <avius.ravis@gmail.com>
2023-03-19 20:47:41 +0100

    added dotnet template

---
+++ source/ECF.Templates/templates/ECF/Program.cs
@@ -0,0 +1,12 @@
+﻿using ECF;
+
+new ECFHostBuilder()
+    .UseDefaultCommands() // register all commands with CommandAttribute and default commands (help, exit, ...)
+    .AddConfiguration(optional: true) // adds appsettings.json  
+    .Configure((ctx, services, _) =>
+    {
+        ctx.Intro = $"This is example console application based on ECF. Version {typeof(Program).Assembly.GetName().Version}.\nType help to list available commands";
+        ctx.HelpIntro = "Welcome to example program that showcases ECF framework. Enter one of command listed below";
+        ctx.Prefix = ">";
+    })
+    .Run(args);
