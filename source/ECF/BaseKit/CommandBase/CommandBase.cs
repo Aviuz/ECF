9b70532e8f7eb192de5a6caa93ca7b410324d8a6
Lew Szymański <lew.szymanski@gmail.com>
2024-08-13 18:05:34 +0200

    format

--- source/ECF/BaseKit/CommandBase/CommandBase.cs
+++ source/ECF/BaseKit/CommandBase/CommandBase.cs
@@ -4,10 +4,7 @@ public abstract class CommandBase : AsyncCommandBase
 {
     public override sealed Task ExecuteAsync(CancellationToken ct)
     {
-        ct.Register(() => {
-            Environment.Exit(1);
-        });
-
+        ct.Register(() => Environment.Exit(1));
         Execute();
         return Task.CompletedTask;
     }
