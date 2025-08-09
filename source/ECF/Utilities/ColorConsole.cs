20b92d1b21da2f5ebb6d8481abcb0c9d2b32cede
Avius <avius.ravis@gmail.com>
2025-08-09 19:11:17 +0200

    removed ColorConsole obsolete methods

--- source/ECF/Utilities/ColorConsole.cs
+++ source/ECF/Utilities/ColorConsole.cs
@@ -39,43 +39,4 @@ public static class ColorConsole
         Console.ForegroundColor = oldFColor;
         Console.BackgroundColor = oldBColor;
     }
-
-    [Obsolete("WriteLineYellow is obsolete. Use WriteLine(string, ConsoleColor) instead.")]
-    public static void WriteLineYellow(string value)
-    {
-        ConsoleColor oldColor = Console.ForegroundColor;
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine(value);
-        Console.ForegroundColor = oldColor;
-    }
-
-    [Obsolete("WriteLineRed is obsolete. Use WriteLine(string, ConsoleColor) instead.")]
-    public static void WriteLineRed(string value)
-    {
-        ConsoleColor oldColor = Console.ForegroundColor;
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine(value);
-        Console.ForegroundColor = oldColor;
-    }
-
-    [Obsolete("WriteYellow is obsolete. Use Write(string, ConsoleColor) instead.")]
-    public static void WriteYellow(string value)
-    {
-        ConsoleColor oldColor = Console.ForegroundColor;
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.Write(value);
-        Console.ForegroundColor = oldColor;
-    }
-
-    [Obsolete("WriteWhiteOnGreen is obsolete. Use Write(string, ConsoleColor, ConsoleColor) instead.")]
-    public static void WriteWhiteOnGreen(string value)
-    {
-        ConsoleColor oldFColor = Console.ForegroundColor;
-        ConsoleColor oldBColor = Console.BackgroundColor;
-        Console.ForegroundColor = ConsoleColor.White;
-        Console.BackgroundColor = ConsoleColor.DarkGreen;
-        Console.Write(value);
-        Console.ForegroundColor = oldFColor;
-        Console.BackgroundColor = oldBColor;
-    }
 }
